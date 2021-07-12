ea42a9c88f59a77d04e75d7efe96588b7e89d202
kor0sensei <averyolmstead@comcast.net>
2021-07-12 13:56:15 -0500

    data now displays from api in DOM, is routed with navbar

---
+++ src/components/nav/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import "./Navbar.css"
+
+export const NavBar = (props) => {
+    return (
+        <ul className="navbar">
+            <li className="navbar__item active">
+                <Link className="navbar__link" to="/">Kandy Korner</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="/products">Products</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="/locations">Locations</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="/customers">Customers</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="/employees">Employees</Link>
+            </li>
+        </ul>
+    )
+}
